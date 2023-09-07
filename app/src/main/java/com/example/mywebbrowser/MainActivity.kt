0e2781c940ea7205cd12d4daf38f39452e674daa
leejinseok <sonaky47@gmail.com>
2023-09-07 09:32:45 +0900

    WebView 생성

--- app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -1,26 +1,42 @@
 package com.example.mywebbrowser
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+            HomeScreen(viewModel = viewModel)
 
         }
     }
@@ -28,7 +44,11 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: MainViewModel) {
+    val (inputValue, setInputValue) = rememberSaveable {
+        mutableStateOf("https://www.google.com")
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -45,11 +65,39 @@ fun HomeScreen() {
         },
     ) {
         Column(
-            modifier = Modifier.padding(it)
+            modifier = Modifier
+                .padding(it)
+                .padding(16.dp)
+                .fillMaxSize()
         ) {
+            OutlinedTextField(
+                value = inputValue,
+                onValueChange = setInputValue,
+                label = { Text(text = "https://") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions(onSearch = {
 
+                })
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            MyWebView(viewModel)
         }
     }
 }
+
+@Composable
+fun MyWebView(viewModel: MainViewModel) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = {
+            WebView(it)
+        },
+        update = { webView ->
+            webView.loadUrl(viewModel.url.value)
+        }
+    )
+}
+
 
 
