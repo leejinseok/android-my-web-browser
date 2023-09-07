278f9d6cef06383b79cdc8eed97690a02bd465c0
leejinseok <sonaky47@gmail.com>
2023-09-07 09:42:16 +0900

    undo, redo action

--- app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -25,12 +25,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +50,8 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
+    val focusManager = LocalFocusManager.current
+
     val (inputValue, setInputValue) = rememberSaveable {
         mutableStateOf("https://www.google.com")
     }
@@ -55,10 +61,12 @@ fun HomeScreen(viewModel: MainViewModel) {
             TopAppBar(
                 title = { Text(text = "나만의 웹 브라우저") },
                 actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = {
+                        viewModel.undo()
+                    }) {
                         Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
                     }
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = { viewModel.redo() }) {
                         Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
                     }
                 }
@@ -78,7 +86,8 @@ fun HomeScreen(viewModel: MainViewModel) {
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
                 keyboardActions = KeyboardActions(onSearch = {
-
+                    viewModel.url.value = inputValue
+                    focusManager.clearFocus()
                 })
             )
             Spacer(modifier = Modifier.height(16.dp))
@@ -89,12 +98,31 @@ fun HomeScreen(viewModel: MainViewModel) {
 
 @Composable
 fun MyWebView(viewModel: MainViewModel) {
+    val coroutineScope = rememberCoroutineScope()
+
     AndroidView(
         modifier = Modifier.fillMaxSize(),
         factory = {
             val webView = WebView(it)
             webView.settings.javaScriptEnabled = true
             webView.webViewClient = WebViewClient()
+
+            coroutineScope.launch {
+                viewModel.undoSharedFlow.collectLatest {
+                    if (webView.canGoBack()) {
+                        webView.goBack()
+                    }
+                }
+            }
+
+            coroutineScope.launch {
+                viewModel.redoSharedFlow.collectLatest {
+                    if (webView.canGoForward()) {
+                        webView.goForward()
+                    }
+                }
+            }
+
             webView
         },
         update = { webView ->
